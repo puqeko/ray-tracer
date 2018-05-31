359e278730033a2c14c879320a0603bbc5c78cdd
puqeko <puqeko@gmail.com>
2018-05-31 23:19:43 +1200

    -

--- lib/Torus.cpp
+++ lib/Torus.cpp
@@ -28,23 +28,6 @@ float Torus::intersect(glm::vec3 posn, glm::vec3 dir)
     double r = this->radius;
 
     // equation for a torus intersection with major radius = 3 * minor radius
-
-    // flat
-    // double a = d.x*d.x*d.x*d.x + d.y*d.y*d.y*d.y + d.z*d.z*d.z*d.z;
-    // a += 2*d.x*d.x*d.y*d.y + 2*d.x*d.x*d.z*d.z + 2*d.y*d.y*d.z*d.z;
-    // double b = 4*d.x*d.x*d.x*p.x + 4*d.x*d.x*d.y*p.y + 4*d.x*d.x*d.z*p.z + 4*d.x*d.y*d.y*p.x + 4*d.x*d.z*d.z*p.x;
-    // b += 4*d.y*d.y*d.y*p.y + 4*d.y*d.y*d.z*p.z + 4*d.y*d.z*d.z*p.y + 4*d.z*d.z*d.z*p.z;
-    // double c = -20*d.x*d.x*r*r + 6*d.x*d.x*p.x*p.x + 2*d.x*d.x*p.y*p.y + 2*d.x*d.x*p.z*p.z;
-    // c += 8*d.x*d.y*p.x*p.y + 8*d.x*d.z*p.x*p.z + 16*d.y*d.y*r*r + 2*d.y*d.y*p.x*p.x + 6*d.y*d.y*p.y*p.y;
-    // c += 2*d.y*d.y*p.z*p.z + 8*d.y*d.z*p.y*p.z - 20*d.z*d.z*r*r + 2*d.z*d.z*p.x*p.x;
-    // c += 2*d.z*d.z*p.y*p.y + 6*d.z*d.z*p.z*p.z;
-    // double d_ = -40*d.x*r*r*p.x + 4*d.x*p.x*p.x*p.x + 4*d.x*p.x*p.y*p.y + 4*d.x*p.x*p.z*p.z;
-    // d_ += 32*d.y*r*r*p.y + 4*d.y*p.x*p.x*p.y + 4*d.y*p.y*p.z*p.z - 40*d.z*r*r*p.z;
-    // d_ += 4*d.z*p.x*p.x*p.z + 4*d.z*p.y*p.y*p.z + 4*d.z*p.z*p.z*p.z;
-    // double e = 64*r*r*r*r - 20*r*r*p.x*p.x + 16*r*r*p.y*p.y - 20*r*r*p.z*p.z;
-    // e += p.x*p.x*p.x*p.x + 2*p.x*p.x*p.y*p.y + 2*p.x*p.x*p.z*p.z + p.y*p.y*p.y*p.y + 2*p.y*p.y*p.z*p.z + p.z*p.z*p.z*p.z;
-
-    // upright
     double a = d.x*d.x*d.x*d.x + d.y*d.y*d.y*d.y + d.z*d.z*d.z*d.z;
     a += 2*d.x*d.x*d.y*d.y + 2*d.x*d.x*d.z*d.z + 2*d.y*d.y*d.z*d.z;
     double b = 4*d.x*d.x*d.x*p.x + 4*d.x*d.x*d.y*p.y + 4*d.x*d.x*d.z*p.z + 4*d.x*d.y*d.y*p.x + 4*d.x*d.z*d.z*p.x;
@@ -59,8 +42,6 @@ float Torus::intersect(glm::vec3 posn, glm::vec3 dir)
     double e = 64*r*r*r*r - 20*r*r*p.x*p.x - 20*r*r*p.y*p.y + 16*r*r*p.z*p.z;
     e += p.x*p.x*p.x*p.x + 2*p.x*p.x*p.y*p.y + 2*p.x*p.x*p.z*p.z + p.y*p.y*p.y*p.y + 2*p.y*p.y*p.z*p.z + p.z*p.z*p.z*p.z;
 
-    // issue with pow
-
     std::complex<double> coeff[5] = {e + 0i, d_ + 0i, c + 0i, b + 0i, a + 0i};
     std::complex<double> sols[4];
     solve_quartic(coeff, sols);
@@ -71,18 +52,6 @@ float Torus::intersect(glm::vec3 posn, glm::vec3 dir)
             roots.push_back(sols[i].real());
         }
     }
-
-    // std::complex<double>* sols = solve_quartic(b/a, c/a, d_/a, e/a);
-    // std::vector<double> roots;
-
-    // // select real and +ve only
-    // for (int i = 0; i < 4; i++) {
-    //     if (abs(sols[i].imag()) < 0.001 && sols[i].real() > 0) {  // sols[i].real() > 0.001
-    //         roots.push_back(sols[i].real());
-    //     }
-    // }
-    // delete[] sols;  // clear memory
-
 
     // no real +ve roots?
     if (roots.empty()) return -1.0;
